0f1cc6b3dbd142dd8da8f45465da3ce0677e7406
Kirill <tveek@hotmail.com>
2018-07-11 00:39:12 +0300

    finished reveal b

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,8 +1,13 @@
 import slider from './modules/slider';
 import Mobile from './modules/mobile';
+import Reveal from './modules/reveal';
 
 
 var mobile = new Mobile();
 var Slider = new slider(); 
+// var reveal = new Reveal();
+new Reveal(document.querySelectorAll('.tastes-section-item'), '50%');
+new Reveal(document.querySelectorAll('.slider-section-item'), '50%');
+new Reveal(document.querySelectorAll('.testimonials__item'), '60%');
 
 
