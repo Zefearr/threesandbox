e5559ce061f28f2128d81f4958ad537777ab8388
Kirill <tveek@hotmail.com>
2018-07-16 03:22:54 +0300

    add build

---
+++ gulp/tasks/build.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp'),
+    imageMin = require('gulp-imagemin'),
+    del = require('del'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cssnano = require('gulp-cssnano'),
+    uglify = require('gulp-uglify'); 
+    browserSync = require('browser-sync').create(); 
+
+    gulp.task('prevDist', function(){
+        browserSync.init({
+            notify: false, 
+            server: {    
+             baseDir: 'docs'    
+          }  
+        }); 
+    })
+
+gulp.task('deleteDist', ['icons'], function(){
+    return del('./docs'); 
+})    
+
+gulp.task('optimizeImg', ['deleteDist'], function(){
+    return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
+        .pipe(imageMin({
+           multipass: true,
+           progressive: true
+        }))
+        .pipe(gulp.dest('./docs/assets/images')); 
+})
+gulp.task('useminTrigger', ['deleteDist'], function(){
+    gulp.start('usemin')
+}) 
+gulp.task('usemin', ['styles', 'scripts'], function(){
+    return gulp.src('./app/index.html')
+        .pipe(usemin({
+            css: [function(){return rev()}, function(){return cssnano()}],
+            js: [function(){return rev()}, function(){return uglify()}] 
+        }))
+        .pipe(gulp.dest('./docs'));
+})
+gulp.task('build', ['deleteDist','optimizeImg', 'useminTrigger']);    
