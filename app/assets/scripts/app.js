e1708bcd6be4ddebc715acc4620f4a8ef1a8ce45
zefear <tveek@hotmail.com>
2019-03-14 21:14:15 +0200

    end

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,17 +1,26 @@
 // import slider from './modules/slider';
-import Mobile from './modules/mobile';
-import Reveal from './modules/reveal';
-import StickyHeader from './modules/stickyheader';
-import Contacts from './modules/contacts'; 
+// import Mobile from './modules/mobile';
+// import Reveal from './modules/reveal';
+// import StickyHeader from './modules/stickyheader';
+// import Contacts from './modules/contacts';  
  
 
-var mobile = new Mobile();
+// var mobile = new Mobile();
 // var Slider = new slider(); 
 // var reveal = new Reveal();
-new Reveal(document.querySelectorAll('.tastes-section-item'), '60%');  
-new Reveal(document.querySelectorAll('.slider-section-item'), '60%');
-new Reveal(document.querySelectorAll('.testimonials__item'), '60%');
-var stickyHeader = new StickyHeader();
-var contacts = new Contacts();
+// new Reveal(document.querySelectorAll('.tastes-section-item'), '60%');  
+// new Reveal(document.querySelectorAll('.slider-section-item'), '60%');
+// new Reveal(document.querySelectorAll('.testimonials__item'), '60%');
+// var stickyHeader = new StickyHeader();
+// var contacts = new Contacts();
+
+
+
+
+
+
+
+  var scene = new THREE.Scene();
+  console.log(scene)
 
 
